589127199b979f47f104a7ba32ea79824674695c
vivienbur <149022066+vivienbur@users.noreply.github.com>
2023-11-01 21:57:47 +0000

    update

--- 8-devinettes_solution.py
+++ 8-devinettes_solution.py
@@ -1,12 +1,12 @@
 import random
 
 # on prend un nombre aléatoire entre 1 et 100
-nombre_aleatoire = random.randint(1, 5)
+nombre_aleatoire = random.randint(1, 100)
 
 nombre_essais = 0
 
 # le joueur a 10 essais pour trouver le nombre aléatoire
-for nombre_essais in range(1, 4):
+for nombre_essais in range(1, 11):
 
   nombre_saisi = int(input("Devine un nombre: "))
 
@@ -20,6 +20,6 @@ for nombre_essais in range(1, 4):
     break
 
 # comment afficher le nombre aléatoire quand on a perdu?
-if(nombre_essais == 3):
+if(nombre_essais == 10):
   print("PERDU!!!")
   print("Le nombre était " + str(nombre_aleatoire))
--- 9-tableaux_2.py
+++ 9-tableaux_2.py
@@ -2,4 +2,19 @@ mon_tableau = [7, 19, 25, 3, 12, 40, 9]
 
 # 1. comment obtenir le plus grand élément du tableau?
 
+maximum = 0
+
+for nombre in mon_tableau:
+  if nombre > maximum:
+    maximum = nombre
+
+print(maximum)
+
 # 2. comment multiplier tous les éléments entre eux?
+
+total = 1
+
+for nombre in mon_tableau:
+  total = total * nombre
+
+print(total)
